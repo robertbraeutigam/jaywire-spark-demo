7aa54a4dc84309c801e6539c2d8fd17d4bc7f8aa
Robert Braeutigam <robert@mathema.de>
2015-02-03 16:32:39 +0100

    Added spark main

---
+++ src/main/java/com/vanillasource/jaywire/demo/web/Main.java
@@ -0,0 +1,26 @@
+package com.vanillasource.jaywire.demo.web;
+
+import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
+
+public class Main {
+   public static final void main(String[] args) {
+      get("/", Main::getHomePage);
+   }
+
+   private static final String getHomePage(Request request, Response response) {
+      return 
+         "<html>"+
+         "   <head><title>JayWire Demo</title></head>"+
+         "   <body>"+
+         "      <h1>Demo Home Page</h1>"+
+         "      <h5>Please choose page from list below</h5>"+
+         "      <ul>"+
+         "         <li><a href=\"/content\">Content Page</a></li>"+
+         "         <li><a href=\"/username\">User Full Name Page</a></li>"+
+         "      </ul>"+
+         "   </body>"+
+         "</html>";
+   }
+}
