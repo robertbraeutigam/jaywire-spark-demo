acb6fec6df174712b703fc9e1e0083590825ea50
Robert Braeutigam <robert@mathema.de>
2015-04-20 16:50:39 +0200

    Moved singleton methods to SingletonSupport

--- src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
+++ src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
@@ -3,25 +3,14 @@ package com.vanillasource.jaywire.demo.service.module;
 import com.vanillasource.jaywire.demo.service.database.*;
 import com.vanillasource.jaywire.demo.service.content.*;
 import com.vanillasource.jaywire.demo.service.user.*;
-import java.util.function.Supplier;
 
-public abstract class ServiceModule {
+public abstract class ServiceModule implements SingletonSupport {
    public Database getDatabase() {
       return singleton( () -> new JdbcDatabase() );
    }
 
    public ContentService getContentService() {
       return singleton( () -> new DatabaseContentService(getDatabase()) );
-   }
-
-   private <T> T singleton(Supplier<T> factory) {
-      return getSingletonStorage().get(factory);
-   }
-
-   public abstract SingletonStorage getSingletonStorage();
-
-   public interface SingletonStorage {
-      <T> T get(Supplier<T> factory);
    }
 
    public UserService getUserService() {
