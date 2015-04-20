74f926f96dd2cae48a7026298ed6fa271c07462c
Robert Braeutigam <robert@mathema.de>
2015-04-20 16:46:08 +0200

    Simplified with supplier, lambda, singleton() method

--- src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
+++ src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
@@ -3,17 +3,25 @@ package com.vanillasource.jaywire.demo.service.module;
 import com.vanillasource.jaywire.demo.service.database.*;
 import com.vanillasource.jaywire.demo.service.content.*;
 import com.vanillasource.jaywire.demo.service.user.*;
+import java.util.function.Supplier;
 
-public class ServiceModule {
-   private Database database = new JdbcDatabase();
-   private ContentService contentService = new DatabaseContentService(getDatabase());
-
+public abstract class ServiceModule {
    public Database getDatabase() {
-      return database;
+      return singleton( () -> new JdbcDatabase() );
    }
 
    public ContentService getContentService() {
-      return contentService;
+      return singleton( () -> new DatabaseContentService(getDatabase()) );
+   }
+
+   private <T> T singleton(Supplier<T> factory) {
+      return getSingletonStorage().get(factory);
+   }
+
+   public abstract SingletonStorage getSingletonStorage();
+
+   public interface SingletonStorage {
+      <T> T get(Supplier<T> factory);
    }
 
    public UserService getUserService() {
