69f125aa283dd6b1db6c6fee4ce76b4af4b15773
Robert Braeutigam <robert@mathema.de>
2015-04-20 19:46:26 +0200

    Implemented session scope support in services

--- src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
+++ src/main/java/com/vanillasource/jaywire/demo/service/module/ServiceModule.java
@@ -5,8 +5,11 @@ import com.vanillasource.jaywire.demo.service.content.*;
 import com.vanillasource.jaywire.demo.service.user.*;
 
 import com.vanillasource.jaywire.SingletonScopeSupport;
+import com.vanillasource.jaywire.Scope;
+import com.vanillasource.jaywire.SessionScopeSupport;
+import java.util.function.Supplier;
 
-public abstract class ServiceModule implements SingletonScopeSupport {
+public abstract class ServiceModule implements SingletonScopeSupport, SessionScopeSupport {
    public Database getDatabase() {
       return singleton( () -> new JdbcDatabase() );
    }
@@ -15,8 +18,8 @@ public abstract class ServiceModule implements SingletonScopeSupport {
       return singleton( () -> new DatabaseContentService(getDatabase()) );
    }
 
-   public UserService getUserService() {
-      return new DatabaseUserService(getDatabase());
+   public Supplier<UserService> getUserService() {
+      return getSessionScope().apply(() -> new DatabaseUserService(getDatabase()));
    }
 }
 
